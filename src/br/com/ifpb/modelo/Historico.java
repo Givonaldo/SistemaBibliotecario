3074d64a44e1bb7deb61929c98d780f60e6391c5
Alexandre Azevedo <azevedo_xandy@hotmail.com>
2015-04-11 00:19:48 -0300

    Adicinando JAR's Externo (Hibernate)

--- src/br/com/ifpb/modelo/Historico.java
+++ src/br/com/ifpb/modelo/Historico.java
@@ -2,6 +2,12 @@ package br.com.ifpb.modelo;
 
 import java.util.Calendar;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 
 @Entity
 @Table(name = "HISTORICOS")
